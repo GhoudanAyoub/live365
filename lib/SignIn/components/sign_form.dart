767f76aa7dca5a194fe208c9a416870616c0728b
ayoub <ayoubghoudanos@gmail.com>
2021-01-24 00:23:17 +0100

    SignIn and Sign Out

---
+++ lib/SignIn/components/sign_form.dart
@@ -0,0 +1,153 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:live365/components/custom_surfix_icon.dart';
+import 'package:live365/components/default_button.dart';
+import 'package:live365/components/form_error.dart';
+import 'package:live365/helper/keyboard.dart';
+
+import '../../SizeConfig.dart';
+import '../../constants.dart';
+
+class SignForm extends StatefulWidget {
+  @override
+  _SignFormState createState() => _SignFormState();
+}
+
+class _SignFormState extends State<SignForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  TextEditingController _emailContoller = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  bool remember = false;
+  final List<String> errors = [];
+
+  void addError({String error}) {
+    if (!errors.contains(error))
+      setState(() {
+        errors.add(error);
+      });
+  }
+
+  void removeError({String error}) {
+    if (errors.contains(error))
+      setState(() {
+        errors.remove(error);
+      });
+  }
+
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+      controller: _passwordController,
+      obscureText: true,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: kPassNullError);
+        } else if (value.length >= 8) {
+          removeError(error: kShortPassError);
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          addError(error: kPassNullError);
+          return "";
+        } else if (value.length < 8) {
+          addError(error: kShortPassError);
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: "Password",
+        hintText: "Enter your password",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Lock.svg"),
+      ),
+    );
+  }
+
+  TextFormField buildEmailFormField() {
+    return TextFormField(
+      controller: _emailContoller,
+      keyboardType: TextInputType.emailAddress,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: kEmailNullError);
+        } else if (emailValidatorRegExp.hasMatch(value)) {
+          removeError(error: kInvalidEmailError);
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          addError(error: kEmailNullError);
+          return "";
+        } else if (!emailValidatorRegExp.hasMatch(value)) {
+          addError(error: kInvalidEmailError);
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: "Email",
+        hintText: "Enter your email",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Mail.svg"),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          buildEmailFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          buildPasswordFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          Row(
+            children: [
+              Checkbox(
+                value: remember,
+                activeColor: kPrimaryColor,
+                onChanged: (value) {
+                  setState(() {
+                    remember = value;
+                  });
+                },
+              ),
+              Text("Remember me"),
+              Spacer(),
+              GestureDetector(
+                onTap: () => Navigator.pushNamed(context, ForgotPasswordScreen.routeName),
+                child: Text(
+                  "Forgot Password",
+                  style: TextStyle(decoration: TextDecoration.underline),
+                ),
+              )
+            ],
+          ),
+          FormError(errors: errors),
+          SizedBox(height: getProportionateScreenHeight(20)),
+          DefaultButton(
+            text: "Continue",
+            press: () async {
+              if (_formKey.currentState.validate()) {
+                KeyboardUtil.hideKeyboard(context);
+                /*
+                dynamic result = await FirebaseService.sign(_emailContoller.text,_passwordController.text,context);
+                if(result!=null){
+                  FirebaseService.checkClientData(context);
+                }*/
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/SignUp/components/body.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:live365/components/socal_card.dart';
+
+import '../../SizeConfig.dart';
+import '../../constants.dart';
+import 'sign_up_form.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.04), // 4%
+                Text("Register Account", style: headingStyle),
+                Text(
+                  "Complete your details or continue \nwith social media",
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                SignUpForm(),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SocalCard(
+                      icon: "assets/icons/google-icon.svg",
+                      press: () async {/*
+                        dynamic result = await FirebaseService.signInWithGoogle(context);
+
+                        if(result!=null){
+                          //Navigator.pushNamed(context, HomeScreen.routeName);
+                        }*/
+                      },
+                    ),
+                    SocalCard(
+                      icon: "assets/icons/facebook-2.svg",
+                      press: () {Scaffold.of(context).showSnackBar(SnackBar(content: Text("Coming Soon")));},
+                    ),
+                    SocalCard(
+                      icon: "assets/icons/twitter.svg",
+                      press: () {Scaffold.of(context).showSnackBar(SnackBar(content: Text("Coming Soon")));},
+                    ),
+                  ],
+                ),
+                SizedBox(height: getProportionateScreenHeight(20)),
+                Text(
+                  'By continuing your confirm that you agree \nwith our Term and Condition',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.caption,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
